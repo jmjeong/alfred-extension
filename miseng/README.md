6fe33bd355e2855e2a5569b4a4658b0c28f391da
Jaemok Jeong <jmjeong@gmail.com>
2013-03-30 00:29:48 +0900

    add miseng extension

---
+++ miseng/README.md
@@ -0,0 +1,21 @@
+## miseng ([Download](https://raw.github.com/jmjeong/alfred-extension/master/miseng/miseng.alfredworkflow))
+
+Comic Viewer - 미생 (Korean Webtoon)
+
+![Demo screenshot](https://raw.github.com/jmjeong/alfred-extension/master/miseng/screenshot.jpg)
+
+###  Usage
+
+```
+ miseng         :: Display index for web-toon
+```
+
+### Version History 
+
+#### 1.2 - March 29, 2013
+
+- Display order is preserved
+
+#### 1.0 - March 27, 2013
+
+- Initial release
---
+++ miseng/main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+# Jaemok Jeong, 2013
+
+import itertools
+import re
+import urllib
+import os
+import alfred
+import uuid
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+url = 'http://cartoon.media.daum.net/webtoon/view/miseng'
+urlRetrieveRe = re.compile(r"data1.push.*img : \"(.*?)\", title:\"(.*?)\",.*url:\"(.*?)\"")
+                              
+ICON = True
+MAX_RESULT = 9
+
+results = []
+
+data = urllib.urlopen(url).read()
+searchPos = 0
+
+for i in xrange(MAX_RESULT):
+    g = urlRetrieveRe.search(data,searchPos)
+    imageUrl = g.group(1)
+    title = g.group(2)
+    suburl = g.group(3)
+    searchPos = g.end()
+
+    filepath = os.path.join(alfred.work(True), os.path.split(imageUrl)[1])
+    if not os.path.exists(filepath):
+        urllib.urlretrieve(imageUrl, filepath)
+
+    results.append(alfred.Item(title=u"미생 :" + title,
+                               subtitle = u"다음 만화속 세상",
+                               attributes={'uid':uuid.uuid4(), 'arg':url+suburl},
+                               icon = filepath))
+alfred.write(alfred.xml(results))
