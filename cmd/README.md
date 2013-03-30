852dc7f4480b13be5a7b004e4ee5a07c813e7762
Jaemok Jeong <jmjeong@gmail.com>
2013-03-30 10:34:50 +0900

    add cmd.alfredextension

---
+++ cmd/README.md
@@ -0,0 +1,23 @@
+## Cmd ([Download](https://raw.github.com/jmjeong/alfred-extension/master/cmd/cmd.alfredworkflow))
+
+Yet another applescript utility made by Jaemok
+
+![Screenshot](https://raw.github.com/jmjeong/alfred-extension/master/cmd/screenshot.jpg)
+
+###  Usage
+
+```
+ cmd            
+```
+
+### Version History 
+
+#### 1.1 - March 30, 2013
+
+- Upload to github
+- Add 'Clean up Duplicate AppleScript Dictionary'
+- apply Alleyoop's auto-updater
+
+#### 1.0 - March 27, 2013
+
+- Initial release
---
+++ cmd/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import alfred
+import uuid
+
+commands = ( ('Hide Hidden Files', 'defaults write com.apple.finder AppleShowAllFiles FALSE; killall Finder'),
+             ('Show Hidden Files', 'defaults write com.apple.finder AppleShowAllFiles TRUE; killall Finder'),
+             ('Fix [open with] duplicate entries', '/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user; killall Finder'),
+             ('Clean up Duplicate AppleScript Dictionary', '/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'),
+            )
+
+results = [alfred.Item(title=t,subtitle=sub,attributes={'uid':uuid.uuid4(),'arg':sub},icon='icon.png') for (t, sub) in commands];
+
+alfred.write(alfred.xml(results))
