231b00d308944f52eb4ec61b312bebef72359c0c
Jaemok Jeong <jmjeong@gmail.com>
2013-06-14 17:03:10 +0900

    add 'download pdf file in safari' menu

--- cmd/main.py
+++ cmd/main.py
@@ -5,6 +5,8 @@ import alfred
 
 commands = ( ('Hide Hidden Files', 'defaults write com.apple.finder AppleShowAllFiles FALSE; killall Finder'),
              ('Show Hidden Files', 'defaults write com.apple.finder AppleShowAllFiles TRUE; killall Finder'),
+             ('Download pdf file in Safari', 'defaults write com.apple.Safari WebKitOmitPDFSupport -bool YES'),
+             ('View pdf file in Safari', 'defaults write com.apple.Safari WebKitOmitPDFSupport -bool NO'),
              ('Fix [open with] duplicate entries', '/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user; killall Finder'),
              ('Clean up Duplicate AppleScript Dictionary', '/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'),
             )
