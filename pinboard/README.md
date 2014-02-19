96ba7424635c5602b0c6ed423a85664c6b0f0aa6
Jaemok Jeong <jmjeong@gmail.com>
2014-02-19 22:05:37 +0900

    pinboard workflow v0.9

---
+++ pinboard/README.md
@@ -0,0 +1,38 @@
+# Introduction 
+
+Yet another alfred2-pinboard workflow. It provides INSTANT pinboard search and various search command.
+
+# Installation 
+
+- (mandatory) install cron job
+	- */10 * * * * (curl 'https://api.pinboard.in/v1/posts/all?format=json&auth_token=username:TOKEN' > ~/.bookmarks.json) > /dev/null 2>&1
+	- get it from [https://pinboard.in/settings/password](https://pinboard.in/settings/password)
+- (optional) pbauth username:TOKEN <- set access token 
+	- Only need if you want to delete the bookmark or to change its status to read
+
+# Command
+
+- pba query : search query from all field
+- pbt query : search query from description(title)
+- pbl query : search query from link
+- pbd query : search query from extended field
+- pbu query : search query from description(title) in unread list
+- pbtag query : search tag list. You can autocomplete it by pressing ‘tab’
+
+# Action
+
+- Open the selected url in the browser by enter
+- Hold *cmd* while selecting a bookmark to copy it’s url to clipboard
+- Hold *ctl* while selecting to delete a bookmark from your pinboard. (not yet)
+- Hold *shift* while selecting to change a bookmark to read. (not yet)
+
+# Change Log 
+
+- v0.9 : pre-release
+
+# Credits
+
+Inspired by the following tips 
+
+- [INSTANT Pinboard Search](https://gist.github.com/myfreeweb/5189568)
+- [Pinboard Search Workflow](http://www.alfredforum.com/topic/979-pinboard-search-workflow/)
---
+++ pinboard/main.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# jmjeong, 2014/02/19
+
+import alfred
+import os
+import json
+import unicodedata
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def intro():
+    result = [alfred.Item(title='Lookup Documentation', subtitle='setup incomplete.', attributes={'arg':'http://jmjeong.com'}, icon="icon.png")]
+    alfred.write(alfred.xml(result))
+
+def tags(pins,query):
+    if not "→" in query:
+        query = query.lower()
+        tag_list = {}
+        for p in pins:
+            tags = p['tags'].encode('utf-8').split(' ')
+            for t in tags:
+                if not t: continue
+                if t in tag_list:
+                    tag_list[t] += 1
+                else:
+                    tag_list[t] = 1
+        resultData = []
+        tag_list_key = sorted(tag_list.keys(),key=str.lower)
+        for i in tag_list_key:
+            if not query or query in i.lower():
+                resultData.append(alfred.Item(title="tag:"+i+"("+str(tag_list[i])+")→", subtitle='', attributes={'arg':i, 'autocomplete':i+"→"}, icon='icon.png'))
+        alfred.write(alfred.xml(resultData,maxresults=None))
+    else:
+        query=query.rstrip("→")
+        results=[]
+        for p in pins:
+            tags = p['tags'].split(' ')
+            url = p['href']
+            for t in tags:
+                if query in t:
+                    results.append({'title':p['description'],'url':url})
+                    break
+        resultData = [alfred.Item(title=f['title'].encode('utf-8'), subtitle=f['url'].encode('utf-8'), attributes = {'arg':f['url']}, icon="item.png") for f in results]
+        alfred.write(alfred.xml(resultData,maxresults=None))
+    sys.exit(0)
+
+try:
+    file = os.environ['HOME']+'/.bookmarks.json'
+    pins = json.loads(open(file, 'r').read())
+except:
+    intro()
+    sys.exit(0)
+
+# arg parsing
+category = sys.argv[1]
+try:
+    q = unicode(sys.argv[2].strip())
+    q = unicodedata.normalize('NFC', q)
+except:
+    q = ""
+
+# tag processing
+if category=='tags':
+    tags(pins,q)
+    sys.exit(0)
+    
+results = []
+q = q.lower()
+
+for p in pins:
+    title = p['description'].replace(' ', '')
+    url = p['href']
+    extended = p['extended'].replace(' ', '')
+    tags = p['tags'].split(' ')
+    toread = p['toread']
+
+    if q=="":
+        if category=='toread':
+            if toread=='yes':            
+                results.append({'title':p['description'],'url':url})
+        else: 
+            results.append({'title':p['description'],'url':url})
+    else:
+        if category=='title' and q in title.lower():
+            results.append({'title':p['description'],'url':url})
+        elif category=='link' and q in url.lower():
+            results.append({'title':p['description'],'url':url})
+        elif category=='description' and q in extended.lower():
+            results.append({'title':p['description'],'url':url})
+        elif category=='toread' and q in title.lower() and toread=='yes':
+            results.append({'title':p['description'],'url':url})
+        elif category=='all' and (q in title.lower() or q in url.lower() or q in extended.lower()):
+            results.append({'title':p['description'],'url':url})
+
+resultData = [alfred.Item(title=f['title'].encode('utf-8'), subtitle=f['url'].encode('utf-8'), attributes = {'arg':f['url']}, icon="item.png") for f in results]
+alfred.write(alfred.xml(resultData,maxresults=None))
