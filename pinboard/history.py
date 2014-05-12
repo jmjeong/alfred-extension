860206c2a3a867c317a3b92e1475f415598b81eb
Jaemok Jeong <jmjeong@gmail.com>
2014-05-12 20:54:59 +0900

    add search in pbhis command

--- pinboard/history.py
+++ pinboard/history.py
@@ -10,18 +10,26 @@ import json
 import main
 import alfred
 import time
+import unicodedata
 
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 if __name__ == '__main__':
+    try:
+        q = unicode(sys.argv[1].strip())
+        q = unicodedata.normalize('NFC', q)
+    except:
+        q = ""
+    
     history = main.history_data()
     history = sorted(history,key=lambda s:s[2],reverse=True)
 
     results = []
     for h in history:
-        results.append(alfred.Item(title=h[0]+" (%d)"%h[1], subtitle=time.strftime('%Y.%m.%d %H:%M:%S', time.localtime(h[2])),
+        if q=="" or q in h[0]:
+            results.append(alfred.Item(title=h[0]+" (%d)"%h[1], subtitle=time.strftime('%Y.%m.%d %H:%M:%S', time.localtime(h[2])),
                                    attributes={'arg':h[0]}, icon="icon.png"))
 
     alfred.write(alfred.xml(results,maxresults=20))
